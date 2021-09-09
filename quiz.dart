a3099d57930ad79f340d5883cd4e06da9a64790b
salma140012 <71036717+salma140012@users.noreply.github.com>
2021-09-09 15:36:01 +0200

    Create quiz.dart

---
+++ quiz.dart
@@ -0,0 +1,9 @@
+import 'package:h/quiz_model/question.dart';
+
+class Quiz{
+  List<Question> questions = [
+    Question('do you have a cool name ?',true),
+    Question('are you happy ?',false),
+    Question('are you enjoying the course ?',true),
+  ];
+}
